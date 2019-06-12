9b04b473b7f9f93e4954572040eb61a56a331536
Li-saltair <huagongyilby@126.com>
2019-06-12 16:36:45 +0800

    基础运算 (#7)
    基础运算 (#7)

--- src/main/java/com/github/hcsp/calculation/Solution.java
+++ src/main/java/com/github/hcsp/calculation/Solution.java
@@ -12,5 +12,10 @@ public class Solution {
     // 4.将上一步的结果除以4
     // 5.将上一步的结果对5取余
     // 将得到的结果返回
-    public static int calculate(int number) {}
+    public static int calculate(int number) {
+        int next = number+1;
+        next = next*2-3;
+        int result = next/4;
+        return Integer.parseInt(String.valueOf(result%5));
+    }
 }
