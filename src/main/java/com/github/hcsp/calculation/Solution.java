fc2eae7e95c233e390252e5e9aae5b2d0ee49c0f
Airtoit <51944383+Airtoit@users.noreply.github.com>
2020-02-28 17:18:28 +0800

    完善calculate方法的计算操作 (#146)
    完善calculate方法的计算操作 (#146)

--- src/main/java/com/github/hcsp/calculation/Solution.java
+++ src/main/java/com/github/hcsp/calculation/Solution.java
@@ -12,5 +12,11 @@ public class Solution {
     // 4.将上一步的结果除以4
     // 5.将上一步的结果对5取余
     // 将得到的结果返回
-    public static int calculate(int number) {}
+    public static int calculate(int number) {
+        number++;
+        number <<= 1;
+        number -= 3;
+        number >>= 2;
+        return number %= 5;
+    }
 }
