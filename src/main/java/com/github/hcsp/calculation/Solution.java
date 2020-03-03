1d5bc154a106bea7e6926c7eda93b415c1652ea2
zhuxueqiao <60607893+zhuxueqiao@users.noreply.github.com>
2020-03-03 23:50:47 +0800

    基本运算符 (#149)
    基本运算符 (#149)

--- src/main/java/com/github/hcsp/calculation/Solution.java
+++ src/main/java/com/github/hcsp/calculation/Solution.java
@@ -12,5 +12,14 @@ public class Solution {
     // 4.将上一步的结果除以4
     // 5.将上一步的结果对5取余
     // 将得到的结果返回
-    public static int calculate(int number) {}
+    public static int calculate(int number) {
+        //传统算法
+    /*   number++;
+        int number2=number*2;
+        int number3=number2-3;
+        int number4=number3/4;
+        int number5 =number4%5;*/
+        //简化算法（嵌套）；
+        return  ((((number+1)*2)-3)/4)%5;
+    }
 }
