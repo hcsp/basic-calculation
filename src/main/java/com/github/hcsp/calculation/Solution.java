9f27f3e10ce2f3bc4b474dd5faf11928a30e141e
liulian1999 <61763395+liulian1999@users.noreply.github.com>
2020-03-10 18:34:45 -0700

    1 (#156)
    1 (#156)

--- src/main/java/com/github/hcsp/calculation/Solution.java
+++ src/main/java/com/github/hcsp/calculation/Solution.java
@@ -4,6 +4,7 @@ public class Solution {
     public static void main(String[] args) {
         // 结果应该是4
         System.out.println(calculate(100));
+
     }
 
     // 1.首先将该数字加1
@@ -12,5 +13,7 @@ public class Solution {
     // 4.将上一步的结果除以4
     // 5.将上一步的结果对5取余
     // 将得到的结果返回
-    public static int calculate(int number) {}
+    public static int calculate(int number) {
+        return (((+ + number * 2) - 3) / 4) % 5;
+    }
 }
