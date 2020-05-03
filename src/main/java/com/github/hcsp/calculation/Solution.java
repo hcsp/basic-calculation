059fc865b2fc93a3abd96bbae1344a4ee83e4fc6
DWB-Frank <57584830+DWB-Frank@users.noreply.github.com>
2020-05-03 23:00:25 +0900

    my commit (#178)
    my commit (#178)

--- src/main/java/com/github/hcsp/calculation/Solution.java
+++ src/main/java/com/github/hcsp/calculation/Solution.java
@@ -12,5 +12,10 @@ public class Solution {
     // 4.将上一步的结果除以4
     // 5.将上一步的结果对5取余
     // 将得到的结果返回
-    public static int calculate(int number) {}
+    public static int calculate(int number) {
+        number += 1;
+        number -= 3;
+        number /= 4;
+        return number % 5;
+    }
 }
