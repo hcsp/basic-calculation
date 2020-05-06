eab3cd59e1b5bc9f5e7fb389c2c36667742beaf8
lr6 <36.zhibo@gmail.com>
2020-05-06 17:58:57 +0800

    进行基础运算

--- src/main/java/com/github/hcsp/calculation/Solution.java
+++ src/main/java/com/github/hcsp/calculation/Solution.java
@@ -12,5 +12,12 @@ public class Solution {
     // 4.将上一步的结果除以4
     // 5.将上一步的结果对5取余
     // 将得到的结果返回
-    public static int calculate(int number) {}
+    public static int calculate(int number) {
+        int t = number + 1;
+        int t1 = t * 2;
+        int t2 = t1 - 3;
+        double t3 = (double) t2 / 4;
+        Double t5 = t3 % 5;
+        return t5.intValue();
+    }
 }
