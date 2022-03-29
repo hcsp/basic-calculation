f00094419304f3e713bcbbb6b0d3c438cea96952
lx <iamxliu@163.com>
2022-03-29 13:53:48 +0800

    333

--- src/main/java/com/github/hcsp/calculation/Solution.java
+++ src/main/java/com/github/hcsp/calculation/Solution.java
@@ -13,7 +13,7 @@ public class Solution {
     // 5.将上一步的结果对5取余
     // 将得到的结果返回
     public static int calculate(int number) {
-        int i = ((number + 1) * 2 -3)/4%5;
+        int i = ((number + 1) * 2 - 3) / 4 % 5;
         return i;
     }
 }
