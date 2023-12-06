392e18180140e95be192ac40c71f74d2896a9a3b
Leo Cheung <leozhang621@gmail.com>
2023-12-06 15:47:53 +0800

    feat: completed missions

--- src/main/java/com/github/hcsp/calculation/Solution.java
+++ src/main/java/com/github/hcsp/calculation/Solution.java
@@ -12,5 +12,11 @@ public class Solution {
     // 4.将上一步的结果除以4
     // 5.将上一步的结果对5取余
     // 将得到的结果返回
-    public static int calculate(int number) {}
+    public static int calculate(int number) {
+        int result1 = number + 1;
+        int result2 = result1 * 2;
+        int result3 = result2 - 3;
+        int result4 = result3 / 4;
+        return result4 % 5;
+    }
 }
